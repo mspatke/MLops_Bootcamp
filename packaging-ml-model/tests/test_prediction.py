529a58a8e0e11cf97bca0827589019f1eeea7b10
mspatke <harshadapatke@gmail.com>
2024-06-09 13:32:12 +0100

    test_predictions added

--- packaging-ml-model/tests/test_prediction.py
+++ packaging-ml-model/tests/test_prediction.py
@@ -8,8 +8,8 @@ PACKAGE_ROOT =  Path(os.path.abspath(os.path.dirname(__file__))).parent
 sys.path.append(str(PACKAGE_ROOT))
 
 from prediction_model.config import config
-from prediction_model.processing.data_handling import load_dataset
-from prediction_model.predict import generate_predictions
+from prediction_model.processing.data_handling import load_datasets
+from prediction_model.predict import generate_prediction
 
 #output from predict is not null
 #output from predict script is str data type
@@ -18,7 +18,19 @@ from prediction_model.predict import generate_predictions
 #Fixtures --> functions before test funstion --> ensure single_predictions
 
 @pytest.fixture
-def single_predictions():
-    test_dataset = load_dataset(config.TEST_FILE)
-    
+def single_prediction():
+    test_dataset = load_datasets(config.TEST_FILE)
+    single_row = test_dataset[:1]
+    result = generate_prediction(single_row)
+    return result
+
+def test_single_pred_not_none(single_prediction):
+    assert single_prediction['predictions'] is not None
+
+
+def test_single_pred_str_type(single_prediction): #data type is string
+    assert isinstance(single_prediction.get('predictions')[0], str)
+
+def test_single_pred_validate(single_prediction): #check the output is Y
+    assert single_prediction.get('predictions')[0] == 'Y'
 
